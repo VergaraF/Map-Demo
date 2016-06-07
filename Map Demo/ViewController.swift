188bfa5db0a59ea716d95fefdd2c5cb629896760
Fabian Vergara <vergarf@gmail.com>
2016-06-06 22:02:16 -0400

    Setting things up, and playing around

--- Map Demo/ViewController.swift
+++ Map Demo/ViewController.swift
@@ -7,11 +7,33 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet var map: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //It could be done in less lines but as I am learning I rather see everything like this, for now.
+        
+        var latitude : CLLocationDegrees = 40.7
+        var longitude: CLLocationDegrees = -73.9
+        
+        var latDelta : CLLocationDegrees = 0.01
+        var longDelta: CLLocationDegrees = 0.01
+        
+        var span : MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        
+        var location : CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        var region : MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        map.setRegion(region, animated: true)
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
